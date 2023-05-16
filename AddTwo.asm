5e61ce796c7cc03b20fedabd8dd0bf32e00c897d
Tweety <taekratt@oregonstate.edu>
2023-05-16 16:12:15 -0700

    Building Introduction

--- AddTwo.asm
+++ AddTwo.asm
@@ -16,7 +16,8 @@ ExitProcess proto,dwExitCode:dword
 
 .data				;DS register
 	intro1 BYTE "Composite Numbers Programmed by Euclid by Tichakorn(Tweety) Taekratok, Kunal Chopra, Hsun-Yu Kuo and Euclid",0							;Introduction set1
-	intro2 BYTE "Enter the number of composite numbers you would like to see. I’ll accept orders for up to 400 composites.",0							;Introduction set2
+	intro2 BYTE "Enter the number of composite numbers you would like to see. I will accept orders for up to 400 composites.",0							;Introduction set2
+	Ask BYTE "Enter the number of composites to display [1 .. 400]: ",0																					;Ask for input
 	endMessage BYTE "Results certified by Euclid. Goodbye.",0																							;end message
 	
 
@@ -24,6 +25,19 @@ ExitProcess proto,dwExitCode:dword
 main proc
 
 	Start:
+		call introduction
+
+
+	ExitSequence:
+		call Crlf
+		mov	EDX, OFFSET endMessage
+		call WriteString
+		call Crlf
+		invoke ExitProcess,0
+		main endp
+		end main
+
+introduction proc
 		;Introduction
 		mov	EDX, OFFSET intro1				
 		call WriteString
@@ -32,18 +46,4 @@ main proc
 		mov	EDX, OFFSET intro2				
 		call WriteString
 		call Crlf
-
-
-	ExitSequence:
-		call Crlf
-		mov	EDX, OFFSET endMessage
-		call WriteString
-		call Crlf
-		mov	EDX, OFFSET byeName
-		call WriteString
-		mov	EDX, OFFSET uName
-		call WriteString
-		call Crlf
-		invoke ExitProcess,0
-		main endp
-		end main
+end introduction

4eb75336d27462fe1762ae6dbe87e0d39e66b772
Tweety <taekratt@oregonstate.edu>
2023-05-17 13:24:02 -0700

    Error Handling

--- AddTwo.asm
+++ AddTwo.asm
@@ -16,10 +16,12 @@ ExitProcess proto,dwExitCode:dword
 
 .data				;DS register
 	intro1 BYTE "Composite Numbers Programmed by Tichakorn(Tweety) Taekratok, Kunal Chopra, Hsun-Yu Kuo and Euclid",0							;Introduction set1
-	intro2 BYTE "Enter the number of composite numbers you would like to see. I will accept orders for up to 400 composites.",0							;Introduction set2
-	Ask BYTE "Enter the number of composites to display [1 .. 400]: ",0																					;Ask for input
-	endMessage BYTE "Results certified by Euclid. Goodbye.",0																							;end message
-	
+	intro2 BYTE "Enter the number of composite numbers you would like to see. I will accept orders for up to 400 composites.",0					;Introduction set2
+	Ask BYTE "Enter the number of composites to display [1 .. 400]: ",0																			;Ask for input
+	endMessage BYTE "Results certified by Euclid. Goodbye.",0																					;end message
+	errorMessage  BYTE "Out of range.",0																										;error message
+	numterm DD ?																																;Number of term
+	boolean DD ?																																;Boolean
 
 .code				;CS register
 
@@ -32,16 +34,62 @@ introduction proc
 		mov	EDX, OFFSET intro2				
 		call WriteString
 		call Crlf
+		call Crlf
 
 		ret
 
 introduction endp
+
+Askfortheterm proc
+	Questionterm:
+		mov	EDX, OFFSET Ask				
+		call WriteString
+		call ReadInt
+		mov numterm, EAX
+
+		call Validate
+
+		mov ECX,1
+		cmp ECX,boolean
+		je Questionterm
+
+		ret
+
+Askfortheterm endp
+
+Validate proc
+	ErrorCheck:
+		mov EAX, numterm
+		mov EBX, 400
+		mov EDX, 1
+		cmp EAX, EBX
+		jg ErrorRespond
+		cmp EAX, EDX
+		jl ErrorRespond
+		mov ECX, 0
+		mov boolean,ECX
+		jmp notError
+
+	;there is an error
+	ErrorRespond:
+		mov	EDX, OFFSET errorMessage				
+		call WriteString
+		call Crlf
+		mov ECX, 1
+		mov boolean, ECX
+
+	;no error
+	notError:
+
+		ret
+Validate endp
 
 
 main proc
 
 	Start:
 		call introduction
+		call Askfortheterm
 
 
 	ExitSequence:
